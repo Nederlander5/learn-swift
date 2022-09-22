ddb7a16b221ac93e0c585f01fb12fcc19fecf212
Nederlander5 <83482134+Nederlander5@users.noreply.github.com>
2022-09-23 01:41:20 +0200

    Update Quadratic.swift

--- 2-variables/quadratic-formula/Quadratic.swift
+++ 2-variables/quadratic-formula/Quadratic.swift
@@ -17,3 +17,16 @@ root2 = (-b - (b*b - 4*a*c).squareRoot()) / (2*a)
 // Outputting the roots
 print("Root 1 is \(root1)")
 print("Root 2 is \(root2)")
+
+========
+var a: Double = 2.0
+var b: Double = 5.0
+var c: Double = 3.0
+var x1: Double = 0
+var x2: Double = 0
+var delta: Double = ((b*b) - (4*a*c)) * ((b*b) - 4*a*c)
+delta = delta.squareRoot()
+x1 = (-b - delta)/(2*a)
+x2 = (-b + delta)/(2*a)
+print("x1 is \(x1)")
+print("X2 is \(x2)")
